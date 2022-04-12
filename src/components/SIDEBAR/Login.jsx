0ce40ee21b5cca11b7e550e1d2c06af07b10ad92
danielforkner <daniel.forkner@gmail.com>
2022-04-12 15:10:57 -0400

    can now register user

--- src/components/SIDEBAR/Login.jsx
+++ src/components/SIDEBAR/Login.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { Modal, Button } from 'react-bootstrap';
+import { loginUser, registerUser } from '../../api/fetch';
 
 const Login = ({ openLogin, setOpenLogin }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [isRegister, setIsRegister] = useState(false);
 
   const handleHide = () => {
     setOpenLogin(false);
@@ -11,7 +13,15 @@ const Login = ({ openLogin, setOpenLogin }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // CALL LOGIN FUNCTION FROM FETCH.JS
+
+    if (isRegister) {
+      const response = await registerUser(username, password);
+      console.log(response);
+      setIsRegister(false);
+    } else {
+      const response = await loginUser(username, password);
+      console.log(response);
+    }
     setUsername('');
     setPassword('');
   };
@@ -19,7 +29,7 @@ const Login = ({ openLogin, setOpenLogin }) => {
   return (
     <Modal show={openLogin} onHide={handleHide} className="loginModalContainer">
       <Modal.Header>
-        <Modal.Title as="h3">Login</Modal.Title>
+        <Modal.Title as="h3">{isRegister ? `Register` : `Login`}</Modal.Title>
       </Modal.Header>
       <Modal.Body className="loginModalBody">
         <form>
@@ -39,6 +49,14 @@ const Login = ({ openLogin, setOpenLogin }) => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          {isRegister ? null : (
+            <Button
+              style={{ margin: '10px 0' }}
+              onClick={() => setIsRegister(true)}
+            >
+              Register New user
+            </Button>
+          )}
           <Button style={{ margin: '10px 0' }} onClick={handleSubmit}>
             Submit
           </Button>
