6b44f91d1e8398557627aec10fda15d32be9d0a2
Josh S Wilkinson <JoshsWilkinson@Outlook.com>
2022-04-12 10:06:54 -0500

    my routines and all activities fetch

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,7 +1,39 @@
+require('dotenv').config();
+const apiURL = process.env.API_URL
+//----------------------------------------------------------------
 export const getRoutines = async () => {
   const response = await fetch(
-    'http://fitnesstrac-kr.herokuapp.com/api/routines'
-  );
+    `${apiURL}/routines`
+    , {
+      headers: {
+        'Content-Type': 'application/json',
+      }})
   const data = await response.json();
   return data;
 };
+//----------------------------------------------------------------
+export const getMyRoutines = async (username) => {
+const response = await fetch(    
+  `${apiURL}/${username}/routines`
+  , {
+    headers: {
+      'Content-Type': 'application/json',
+    }})
+const data = await response.json();
+return data;
+
+}
+//----------------------------------------------------------------
+export const getActivities = async () => {
+  const response = await fetch(    
+    `${apiURL}/activities`
+    , {
+      method: "POST",
+      body: JSON.stringify({
+        name: 'Running',
+        description: 'Keep on running!'
+      })})
+  const data = await response.json();
+  return data;
+
+  }
