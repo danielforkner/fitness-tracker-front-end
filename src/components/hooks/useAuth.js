ff57f22c9cfdf14c860020c71390bd7da071b0f1
danielforkner <daniel.forkner@gmail.com>
2022-04-13 14:13:00 -0400

    dropdown of all activities added to routine_details component

--- src/components/hooks/useAuth.js
+++ src/components/hooks/useAuth.js
@@ -4,13 +4,16 @@ import AuthContext from '../../AuthContext';
 // const context = useContext(AuthContext)
 
 const useAuth = () => {
-  const { user, setUser, token, setToken } = useContext(AuthContext);
+  const { user, setUser, token, setToken, allActivities, setAllActivities } =
+    useContext(AuthContext);
 
   return {
     user,
     setUser,
     token,
     setToken,
+    allActivities,
+    setAllActivities,
   };
 };
 
