d9778ad954c2f29498a442f336f39fc023011c20
danielforkner <daniel.forkner@gmail.com>
2022-04-14 18:17:25 -0400

    reactivated logout

--- src/components/SIDEBAR/Login_Button.jsx
+++ src/components/SIDEBAR/Login_Button.jsx
@@ -16,13 +16,24 @@ const Login_Button = ({ setOpenLogin }) => {
   return (
     <div id="loginCard" className="col col-1 card">
       <i className="NavIcon bi bi-person-lines-fill"></i>
-      <h1 className="ftHeader loginHeader card-title"> {user.username ? `Hello, ${user.username}` : 'Please Log In'}</h1>
+      <h1 className="ftHeader loginHeader card-title">
+        {' '}
+        {user.username ? `Hello, ${user.username}` : 'Please Log In'}
+      </h1>
       {user.username ? (
-        <div id="logOutBtn" className="loginButton ftIcon btn btn-outline-secondary">
+        <div
+          id="logOutBtn"
+          className="loginButton ftIcon btn btn-outline-secondary"
+          onClick={logOut}
+        >
           LOG OUT
         </div>
       ) : (
-        <div id="loginBtn" className="loginButton ftIcon btn btn-outline-secondary" onClick={logIn}>
+        <div
+          id="loginBtn"
+          className="loginButton ftIcon btn btn-outline-secondary"
+          onClick={logIn}
+        >
           LOG IN
         </div>
       )}
