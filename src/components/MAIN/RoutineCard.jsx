ff7f7e924dccb2b61f7cea1da9c95898d94cb803
danielforkner <daniel.forkner@gmail.com>
2022-04-15 11:25:50 -0400

    routineCard

--- src/components/MAIN/RoutineCard.jsx
+++ src/components/MAIN/RoutineCard.jsx
@@ -8,6 +8,9 @@ const RoutineCard = ({ routine, setCurentRoutine, setOpen }) => {
   return (
     <div className="routineCard card text-center">
       <h4 className="ftHeader card-title">{routine.name}</h4>
+      <p>
+        <em className="ftHeader">{`By ${routine.creatorName}`}</em>
+      </p>
       <div className="card-body">
         <p className="ftBodyText card-text">{routine.goal}</p>
         <div onClick={handleClick} className="btn btn-outline-secondary">
